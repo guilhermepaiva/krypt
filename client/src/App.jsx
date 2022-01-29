8a37244e2652327d7ad304d330170f997aaf56a4
Guilherme Paiva <guilhermepaiva@MacBook-Pro-de-Guilherme.local>
2022-01-29 15:50:29 -0300

    project structure and some initial code

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { Footer, Navbar, Transactions, Services, Welcome } from "./components"
+
+const App = () => {
+
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,12 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+
+import logo from '../../images/logo.png';
+
+const Welcome = () => {
+    return (
+        <h1>Welcome</h1>
+    )
+}
+
+export default Welcome
