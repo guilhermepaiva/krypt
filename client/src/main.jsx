c793b5deca1fc31ee75a3928821042181365649b
Guilherme Paiva <guilhermepaiva@MacBook-Pro-de-Guilherme.local>
2022-01-30 11:05:04 -0300

    client finished

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+import { TransactionsProvider } from './context/TransactionContext'
+
 ReactDOM.render(
+  <TransactionsProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </TransactionsProvider>,
   document.getElementById('root')
 )
---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from './Transactions.json';
+
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x0dE5b95a7C01a1682713e57c016CAC2D0B4052fC';
